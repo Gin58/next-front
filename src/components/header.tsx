5206cedd43b9c09b974e6d517a66779418d342d5
Ginji Oyama <invincible.gnj1875@gmail.com>
2021-01-05 22:42:44 +0900

    add header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,63 @@
 import Link from "next/link";
 
-const items = [
-  { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-];
-
 export const Header = () => {
   return (
-    <header>
-      <h1>Title</h1>
-      <nav>
-        {items.map(({ href, label }) => {
-          return (
-            <Link key={href} href={href}>
-              <a style={{ display: "inline-block", padding: 12 }}>{label}</a>
-            </Link>
-          );
-        })}
+    <div>
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-8 w-8"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                  alt="Workflow"
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <Link href="/#">
+                    <a className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
+                  </Link>
+
+                  <Link href="/#">
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                      Team
+                    </a>
+                  </Link>
+
+                  <Link href="/#">
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                      Projects
+                    </a>
+                  </Link>
+
+                  <Link href="/#">
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                      Calendar
+                    </a>
+                  </Link>
+
+                  <Link href="/#">
+                    <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                      Reports
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <Link href="/#">
+                  <a className="text-gray-300 hover:bg-gray-00 text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Dashboard
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
-    </header>
+    </div>
   );
 };
